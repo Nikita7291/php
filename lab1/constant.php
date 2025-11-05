e2b73818a719fb5f76700c4640cfc67d60b679cd
Nikita <dezgan009228@gmail.com>
2025-11-05 14:48:29 +0300

    Add files via upload

---
+++ lab1/constant.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ЗАДАНИЕ 1
+- Создайте константу и присвойте ей значение.
+*/
+define('MY_CONSTANT', 'PHP');
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Константы</title>
+</head>
+<body>
+	<h1>Константы</h1>
+	<?php
+	/*
+	ЗАДАНИЕ 2
+	- Проверьте, существует ли константа, которую Вы хотите использовать.
+	- Выведите значение созданной константы.
+	ЗАДАНИЕ 3
+	- Используя предопределённые в ядре константы выведите текущую версию PHP.
+	- Используя магические константы выведите директорию скрипта.
+	*/
+	if (defined('MY_CONSTANT')) {
+		echo 'Константа существует!<br>';
+		echo 'Значение константы: ' . MY_CONSTANT . '<br>';
+	} else {
+		echo 'Константа не найдена.<br>';
+	}
+	echo 'Текущая версия PHP: ' . PHP_VERSION . '<br>';
+	echo 'Директория скрипта: ' . __DIR__;
+	
+	?>
+</body>
+</html>
---
+++ lab1/vars.php
@@ -0,0 +1,32 @@
+<?php
+$name = 'Никита';
+$age = 20;
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Переменные и вывод</title>
+</head>
+<body>
+	<h1>Переменные и вывод</h1>
+	<?php
+		echo "Меня зовут: $name<br>";
+		echo "Мне $age лет<br>";
+		echo "Тип переменной \$name: " . gettype($name) . "<br>";
+		echo "Тип переменной \$age: " . gettype($age) . "<br>";
+		unset($name, $age);
+	/*
+	ЗАДАНИЕ 2
+	- Выведите с помощью echo фразу "Меня зовут: $name", например: 'Меня зовут: Иван'.
+	- Выведите фразу "Мне $age лет", например: 'Мне 20 лет'.
+	- Выведите информацию о типе переменных $name и $age.
+	- Удалите переменные $name и $age.
+	- Измените код так, чтобы каждая фраза начиналась с новой строки.
+	- Изолируйте код PHP от HTML-разметки.
+	*/
+	?> 
+</body>
+</html>
